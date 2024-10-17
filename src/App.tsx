fb490a219295ebdd8e9c2f94a420119b796a4d46
Neerajnightcoder <kumar.neeraj.developer@gmail.com>
2024-10-17 18:33:19 +0530

    chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Button } from '@chakra-ui/react'
 
 function App() {
  
-
+return <Button>Add to Cart</Button>
 
 }
 
