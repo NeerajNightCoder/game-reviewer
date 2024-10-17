c1e1fd81f8024eef5e5d2ff065f6edb2c80c32aa
Neerajnightcoder <kumar.neeraj.developer@gmail.com>
2024-10-17 19:45:57 +0530

    built game cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Button, Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/Navbar'
+import GameGrid from './components/GameGrid'
 
 function App() {
  
 return  <Grid templateAreas={{base:`"nav" "main"`,lg:`"nav nav" "aside main"`}}>
 <GridItem area="nav"><NavBar/></GridItem>
-{/* <GridItem area="navv" bg="coral">Navv</GridItem> */}
-<Show above='lg'><GridItem area="aside" bg="orange">Aside</GridItem></Show>
-<GridItem area="main" bg="red">Main</GridItem>
+{/* <GridItem area="navv">Navv</GridItem> */}
+<Show above='lg'><GridItem area="aside" >Aside</GridItem></Show>
+<GridItem area="main" ><GameGrid/></GridItem>
 
 </Grid>
 
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
+    params:{
+        key:'f5debe707b484acbaa5cf9468875651c'
+    }
+})
