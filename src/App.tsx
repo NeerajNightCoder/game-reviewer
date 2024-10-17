3e455ed41ac260e54d7a861bac1f2d7d711adb3c
Neerajnightcoder <kumar.neeraj.developer@gmail.com>
2024-10-17 18:46:45 +0530

    built responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Button } from '@chakra-ui/react'
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
  
-return <Button>Add to Cart</Button>
+return  <Grid templateAreas={{base:`"nav" "main"`,lg:`"nav nav" "aside main"`}}>
+<GridItem area="nav" bg="coral">Nav</GridItem>
+{/* <GridItem area="navv" bg="coral">Navv</GridItem> */}
+<Show above='lg'><GridItem area="aside" bg="orange">Aside</GridItem></Show>
+<GridItem area="main" bg="red">Main</GridItem>
+
+</Grid>
 
 }
 
